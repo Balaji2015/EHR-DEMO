7270ce2ef230a7dc0844fcfce26f9dfbce10c81a
Anurag Singh <Anurag.Singh@india.akidolabs.com>
2023-09-26 17:51:10 +0530

    Fix bug in error method

--- UI/JScripts/JSEandMCodingNew_V1.js
+++ UI/JScripts/JSEandMCodingNew_V1.js
@@ -660,17 +660,18 @@ myapp.controller('EandMCodingCtrl', function ($scope, $http) {
                         if (xhr.status == 999)
                             window.location = xhr.statusText;
                         else {
-                            var log = JSON.parse(xhr.responseText);
-                            console.log(log);
-                            alert("USER MESSAGE:\n" +
-                                ". Cannot process request. Please Login again and retry. \nEXCEPTION DETAILS: \n" +
-                                "Message: " + log.Message);
-                        }
+                            if (isValidJSON(xhr.responseText)) {
+                                var log = JSON.parse(xhr.responseText);
+                                console.log(log);
+                                alert("USER MESSAGE:\n" +
+                                    ". Cannot process request. Please Login again and retry. \nEXCEPTION DETAILS: \n" +
+                                    "Message: " + log.Message);
+                            }
                             else {
                                 alert("USER MESSAGE:\n" +
                                     ". Cannot process request. Please Login again and retry.");
                             }
-                        }
+                        }     
                     }
                 });
             }
