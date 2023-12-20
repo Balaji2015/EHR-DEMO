59e54fbb50d1af608cdd1d96962ef45eaeed9196
Nitin MS <Nitin.Ms@india.akidolabs.com>
2023-12-20 14:57:58 +0530

    CAP-1231

--- UI/frmFindPatient.aspx.designer.cs
+++ UI/frmFindPatient.aspx.designer.cs
@@ -204,6 +204,15 @@ namespace Acurus.Capella.UI
         protected global::System.Web.UI.WebControls.HiddenField hdnFromScreen;
 
         /// <summary>
+        /// hdnDataHumanDetails control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnDataHumanDetails;
+
+        /// <summary>
         /// SelectedItem control.
         /// </summary>
         /// <remarks>
