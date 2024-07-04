d66e556923012b9a6bccdc4ab8ceaf28720549db
Anurag Singh <Anurag.Singh@india.akidolabs.com>
2024-07-04 00:36:50 -0700

    CAP-2248: Direct URL--> The report screen keeps on loading again and again each time when 'close' is clicked.

--- UI/JScripts/JSC5PO.js
+++ UI/JScripts/JSC5PO.js
@@ -3574,6 +3574,11 @@ $("#btnReportClose").click(function () {
         localStorage.setItem("ClearEncounter", "true");
         window.location.href = "frmPatientChart.aspx";
     } else {
+        //CAP-2248
+        var urlParam = new URLSearchParams(window.location.href);
+        isDirectURLParam = urlParam?.get("IsDirectUrl") ?? "";
+        if (isDirectURLParam.toUpperCase() != "Y") {
         location.reload();
+    }
     }
 });
