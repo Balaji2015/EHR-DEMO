943dd3788de626b1e1bb2143fbc85191efad3847
Anurag Singh <Anurag.Singh@india.akidolabs.com>
2024-09-26 04:48:25 -0700

    CAP-2504: Load Iframe after login

--- UI/JScripts/JSLandingScreen.js
+++ UI/JScripts/JSLandingScreen.js
@@ -613,11 +613,14 @@ function EHRLanding(FileName) {
     //}
 
     //localStorage.setItem("PFSHVerified", "");
-
+    //CAP-2504
+    if ((window.location?.origin ?? "") == "https://chart-stage.akidolabs.com") {
+        let iframe = document.querySelector('iframe[data-testid="CapellaRefillIntegrationIFrame"]');
+        iframe.contentWindow.location.href = FileName;
+    }
+    else {
     top.window.location = FileName;
-
-
-
+    }
 }
 
 $(top.window.document).find('#btnErrorCancel').unbind("click");
