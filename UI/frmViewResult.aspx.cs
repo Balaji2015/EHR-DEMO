7b86eb770411f48f22e6337edf162cd163c9b182
SakthibalanSekar <SakthibalanSekar@ACULAP072>
2024-10-07 00:09:39 -0700

    Jira cap - 2369 - Disable notes icon

--- UI/frmViewResult.aspx.cs
+++ UI/frmViewResult.aspx.cs
@@ -1384,7 +1384,9 @@ namespace Acurus.Capella.UI
                     //Jira CAP-2228
                     if (sNotes != null && sNotes.Count() > 0 &&(sNotes.Contains(hdnSubDocumentType.Value.ToUpper())))
                     {
-                        if (Request["Opening_from"] != null && Request["Opening_from"] != "OrderManagementScreen")
+                        //Jira CAP-2369
+                        //if (Request["Opening_from"] != null && Request["Opening_from"] != "OrderManagementScreen")
+                        if (Request["Opening_from"] != null && Request["Opening_from"] != "OrderManagementScreen" && txtMedicalAssistantNotes.Enabled != true)
                         {
                             imgCopyPrevious.Visible = true;
                         }
