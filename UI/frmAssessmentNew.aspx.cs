771bb2805ab8bfa778009777b44c2cd80afa2e0b
Nitin MS <Nitin.Ms@india.akidolabs.com>
2025-01-28 19:50:33 -0800

    CAP-2909 - Individual Copy Previous throwing error message in Assessment Screen

--- UI/frmAssessmentNew.aspx.cs
+++ UI/frmAssessmentNew.aspx.cs
@@ -379,17 +379,17 @@ namespace Acurus.Capella.UI
                         if (Convert.ToUInt32(assessmentLoadList.PEncID) == 0)
                         {
                             jsons = "Message-" + "210010";
-                            return Compress(jsons);
+                            return Compress(JsonConvert.SerializeObject(jsons));
                         }
                         else if (!Convert.ToBoolean(assessmentLoadList.Physician_Process))
                         {
                             jsons = "Message-" + "210016";
-                            return Compress(jsons);
+                            return Compress(JsonConvert.SerializeObject(jsons));
                         }
                         else if (assessmentLoadList.Assessment != null && assessmentLoadList.Assessment.Count == 0)
                         {
                             jsons = "Message-" + "170014";
-                            return Compress(jsons);
+                            return Compress(JsonConvert.SerializeObject(jsons));
                         }
                         else
                         {
