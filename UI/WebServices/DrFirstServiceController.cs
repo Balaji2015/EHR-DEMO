052d56afeec9c0c119613ff959c3e5beb94d932a
Nitin MS <Nitin.Ms@india.akidolabs.com>
2025-08-11 05:24:18 -0700

    CAP-3524 - Capella DrFirst Download API - To download Allergies, Medications and Prescriptions for a patient

--- UI/WebServices/DrFirstServiceController.cs
+++ UI/WebServices/DrFirstServiceController.cs
@@ -1,6 +1,8 @@
 ﻿using Acurus.Capella.DataAccess;
 using Acurus.Capella.DataAccess.ManagerObjects;
 using System;
+using System.Configuration;
+using System.Linq;
 using System.Web.Http;
 
 namespace Acurus.Capella.UI.WebServices.API
@@ -9,25 +11,48 @@ namespace Acurus.Capella.UI.WebServices.API
     public class DrFirstServiceController : ApiController
     {
         [HttpGet]
-        public IHttpActionResult DownloadDrFirstData(ulong HumanID, string FacilityName, string UserName, string LegalOrg)
+        public IHttpActionResult DownloadDrFirstData(string sHumanID, string sFacilityName, string sLegalOrg)
         {
             try
             {
+                if (!VerifyToken())
+                {
+                    return Json(new { status = "Unauthorized", ErrorDescription = "The remote server returned an error: (403) Forbidden." });
+                }
+                if (string.IsNullOrEmpty(sLegalOrg))
+                {
+                    return Json(new { HumanID = sHumanID, status = "ValidationError", ErrorDescription = "LegalOrg is not valid. Cannot download DrFirst data." });
+                }
+                if (string.IsNullOrEmpty(sHumanID) || sHumanID == "0")
+                {
+                    return Json(new { HumanID = sHumanID, status = "ValidationError", ErrorDescription = "HumanID is not valid. Cannot download DrFirst data." });
+                }
+
                 string sErrorMessage = string.Empty;
                 Rcopia_Update_InfoManager objUpdateInfoMngr = new Rcopia_Update_InfoManager();
-                RCopiaSessionManager rcopiaSessionMngr = new RCopiaSessionManager(LegalOrg);
+                RCopiaSessionManager rcopiaSessionMngr = new RCopiaSessionManager(sLegalOrg);
 
-                if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(LegalOrg) && HumanID > 0)
-                {
-                    string downloadAddress = "";
-                    sErrorMessage = objUpdateInfoMngr.DownloadRCopiaInfo(downloadAddress, UserName, string.Empty, DateTime.UtcNow, FacilityName, 0, HumanID, LegalOrg);
-                }
+                string downloadAddress = "";
+                sErrorMessage = objUpdateInfoMngr.DownloadRCopiaInfo(downloadAddress, "AcurusApi", string.Empty, DateTime.UtcNow, sFacilityName, 0, Convert.ToUInt64(sHumanID), sLegalOrg);
             }
             catch (Exception ex)
             {
-                return Json(new { HumanID, status = "Error", ErrorDescription = "Error in processing the request. " + ex.Message });
+                return Json(new { HumanID = sHumanID, status = "Error", ErrorDescription = "Error in processing the request. " + ex.Message });
             }
-            return Json(new { HumanID, status = "Acknowledged" });
+            return Json(new { HumanID = sHumanID, status = "Acknowledged" });
+        }
+
+        private bool VerifyToken()
+        {
+            var authorization = Request.Headers.GetValues("Authorization");
+            string token = authorization.Any() ? authorization.FirstOrDefault() : "";
+            token = token.Replace("Bearer ", "");
+            var endPointToken = ConfigurationSettings.AppSettings["EndPointToken"] ?? "";
+            if (token == null || string.IsNullOrEmpty(token.ToString()) || string.IsNullOrEmpty(endPointToken) || token.ToString() != endPointToken)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
