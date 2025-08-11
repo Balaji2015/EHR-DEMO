a87f7a577a9ad04732b04c290a3de4b063f2c9a8
Nitin MS <Nitin.Ms@india.akidolabs.com>
2025-08-11 04:58:40 -0700

    CAP-3524 - Capella DrFirst Download API - To download Allergies, Medications and Prescriptions for a patient

---
+++ UI/WebServices/DrFirstServiceController.cs
@@ -0,0 +1,33 @@
+﻿using Acurus.Capella.DataAccess;
+using Acurus.Capella.DataAccess.ManagerObjects;
+using System;
+using System.Web.Http;
+
+namespace Acurus.Capella.UI.WebServices.API
+{
+    //CAP-3524
+    public class DrFirstServiceController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult DownloadDrFirstData(ulong HumanID, string FacilityName, string UserName, string LegalOrg)
+        {
+            try
+            {
+                string sErrorMessage = string.Empty;
+                Rcopia_Update_InfoManager objUpdateInfoMngr = new Rcopia_Update_InfoManager();
+                RCopiaSessionManager rcopiaSessionMngr = new RCopiaSessionManager(LegalOrg);
+
+                if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(LegalOrg) && HumanID > 0)
+                {
+                    string downloadAddress = "";
+                    sErrorMessage = objUpdateInfoMngr.DownloadRCopiaInfo(downloadAddress, UserName, string.Empty, DateTime.UtcNow, FacilityName, 0, HumanID, LegalOrg);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { HumanID, status = "Error", ErrorDescription = "Error in processing the request. " + ex.Message });
+            }
+            return Json(new { HumanID, status = "Acknowledged" });
+        }
+    }
+}
