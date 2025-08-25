703a37598b2d04ce5e079f9533fb29a340387ec0
Nitin MS <Nitin.Ms@india.akidolabs.com>
2025-08-24 22:50:43 -0700

    CAP-3457 - iPad - In Testing Link, Service Procedure Code shows error message

--- UI/JScripts/JSEandMCodingNew_V1.js
+++ UI/JScripts/JSEandMCodingNew_V1.js
@@ -1157,12 +1157,12 @@ myapp.controller('EandMCodingCtrl', function ($scope, $http) {
     //        return $(li).attr("value");
     //    });
     //});
-    $.get("htmICD10.html?version=" + localStorage.getItem("ScriptVersion").split('|')[0].trim(), function (data) {
-        $('#dlstICD10').html(data);
-        arrICD10Codes = $.map($('#dlstICD10 option'), function (li) {
-            return $(li).attr("value");
-        });
-    });
+    $("#dlstICD10").load("htmICD10.html?version=" + localStorage.getItem("ScriptVersion").split('|')[0].trim(),
+        function () {
+            var select = document.getElementById("dlstICD10");
+            arrICD10Codes = Array.from(select.options, opt => opt.value);
+        }
+    );
     $("#txtICD10").autocomplete({
         source:
             function (request, response) {
