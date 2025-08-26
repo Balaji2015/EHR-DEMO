432cbcf5c29280accfcf7935bcf2421c1a7a379f
Nitin MS <Nitin.Ms@india.akidolabs.com>
2025-08-25 20:57:53 -0700

    CAP-3456 - iPad - In Testing Link, Assessment tab show error message in page loading

--- UI/JScripts/jsAssessmentNew.js
+++ UI/JScripts/jsAssessmentNew.js
@@ -1348,12 +1348,12 @@ myapp.controller('assessmentCtrl', function ($scope, $http) {
     //    });
 
     //});
-    $.get("htmICD10.html?version=" + localStorage.getItem("ScriptVersion").split('|')[0].trim(), function (data) {
-        $('#dlstICD10').html(data);
-        arrICD10Codes = $('#dlstICD10 option').map(function () {
-            return this.value;
-        }).get();
-    });
+    $("#dlstICD10").load("htmICD10.html?version=" + localStorage.getItem("ScriptVersion").split('|')[0].trim(),
+        function () {
+            var select = document.getElementById("dlstICD10");
+            arrICD10Codes = Array.from(select.options, opt => opt.value);
+        }
+    );
 
     $("#txtICD10").autocomplete({
         source:
