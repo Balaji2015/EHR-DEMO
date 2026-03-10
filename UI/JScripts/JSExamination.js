9d8bd504744c15210c58706c6f801389a7c4fb2b
Varshit Mahavirbhai <Varshit.Mahavirbhai@india.akidolabs.com>
2026-03-10 11:14:20 +0530

    CAP-4174 Unexpected end of JSON input

--- UI/JScripts/JSExamination.js
+++ UI/JScripts/JSExamination.js
@@ -1267,11 +1267,18 @@ function callweb(icon, List, id) {
                 if (xhr.status == 999)
                     window.location = "/frmSessionExpired.aspx";
                 else {
-                    var log = JSON.parse(xhr.responseText);
-                    console.log(log);
-                    alert("USER MESSAGE:\n" +
-                                    ". Cannot process request. Please Login again and retry. \nEXCEPTION DETAILS: \n" +
-                                   "Message: " + log.Message);
+                    //CAP-4174 Unexpected end of JSON input
+                    if (isValidJSON(xhr.responseText)) {
+                        var log = JSON.parse(xhr.responseText);
+                        console.log(log);
+                        alert("USER MESSAGE:\n" +
+                            ". Cannot process request. Please Login again and retry. \nEXCEPTION DETAILS: \n" +
+                            "Message: " + log.Message);
+                    }
+                    else {
+                        alert("USER MESSAGE:\n" +
+                            ". Cannot process request. Please Login again and retry.");
+                    }
                 }
 
                 { sessionStorage.setItem('StartLoading', 'false'); StopLoadFromPatChart(); }
